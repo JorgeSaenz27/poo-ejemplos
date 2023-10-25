38ecdf2da4dd41f29a954ccb8031b747c93f2988
Joprge <jorgess4540@gmail.com>
2023-10-24 21:55:03 -0700

    Se agrega clase Persona en java

---
+++ practicas/unidad2/java/Persona.java
@@ -0,0 +1,50 @@
+package practicas.unidad2.java;
+
+class Persona {
+
+    public static void main(String[] args) {
+
+        Persona persona = new Persona();
+
+        persona.edad = 25;
+        persona.nombre = "Rogelio";
+        persona.vive = true;
+
+        System.out.println("La edad de la persona es " + persona.edad);
+         System.out.println("La persona vive? " + persona.vive);
+
+        persona.saludar();
+
+        System.out.println("La edad de la persona es " + persona.edad);
+         System.out.println("La persona vive? " + persona.vive);
+
+        persona.incrementarEdad();
+
+        System.out.println("La edad de la persona es " + persona.edad);
+         System.out.println("La persona vive? " + persona.vive);
+
+        persona.morir();
+
+        System.out.println("La edad de la persona es " + persona.edad);
+         System.out.println("La persona vive? " + persona.vive);
+
+
+    }
+    String nombre;
+    int edad;
+
+    boolean vive;
+
+    void saludar() {
+    System.out.println("Hola mi nombre es " + nombre);
+    }
+
+    void incrementarEdad(){
+        edad = edad + 1;
+    }
+
+    void morir(){
+        vive = false;
+    }
+
+}
